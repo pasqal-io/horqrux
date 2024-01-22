dd4aa7ada5abad3030a09f47c450cc608231594d
seitzdom <dominik.seitz@pasqal.com>
2024-01-22 15:02:57 +0100

    Remove init

--- horqrux/__init__.py
+++ horqrux/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from .analog import HamiltonianEvolution
 from .parametric import Rx, Ry, Rz
 from .primitive import NOT, SWAP, H, I, S, T, X, Y, Z
 from .utils import prepare_state
