59eedcb906faaa686de991a807c95b72611f1812
seitzdom <dominik.seitz@pasqal.com>
2024-01-29 15:26:28 +0100

    Update docstrings

--- horqrux/apply.py
+++ horqrux/apply.py
@@ -15,38 +15,38 @@ from .utils import OperationType, State, _controlled, is_controlled
 
 def apply_operator(
     state: State,
-    unitary: Array,
+    operator: Array,
     target: Tuple[int, ...],
     control: Tuple[int | None, ...],
 ) -> State:
-    """Applies a unitary, i.e. a single array of shape [2, 2, ...], on a given state
+    """Applies an operator, i.e. a single array of shape [2, 2, ...], on a given state
        of shape [2 for _ in range(n_qubits)] for a given set of target and control qubits.
-       In case of control qubits, the 'unitary' array will be embedded into a controlled array.
+       In case of a controlled operation, the 'operator' array will be embedded into a controlled array.
 
-       Since dimension 'i' in 'state' corresponds to all amplitudes which are affected by qubit 'i',
-       target and control qubits correspond to dimensions to contract 'unitary' over.
-       Contraction over qubit 'i' means applying the 'dot' operation between 'unitary' and dimension 'i'
+       Since dimension 'i' in 'state' corresponds to all amplitudes where qubit 'i' is 1,
+       target and control qubits represent the dimensions over which to contract the 'operator'.
+       Contraction means applying the 'dot' operation between the operator array and dimension 'i'
        of 'state, resulting in a new state where the result of the 'dot' operation has been moved to
        dimension 'i' of 'state'. To restore the former order of dimensions, the affected dimensions
        are moved to their original positions and the state is returned.
 
     Arguments:
         state: State to operate on.
-        unitary: Array to contract over 'state'.
+        operator: Array to contract over 'state'.
         target: Tuple of target qubits on which to apply the 'operator' to.
         control: Tuple of control qubits.
 
     Returns:
-        State after applying 'unitary'.
+        State after applying 'operator'.
     """
     state_dims: Tuple[int, ...] = target
     if is_controlled(control):
-        unitary = _controlled(unitary, len(control))
+        operator = _controlled(operator, len(control))
         state_dims = (*control, *target)  # type: ignore[arg-type]
-    n_qubits = int(np.log2(unitary.size))
-    unitary = unitary.reshape(tuple(2 for _ in np.arange(n_qubits)))
-    op_dims = tuple(np.arange(unitary.ndim // 2, unitary.ndim, dtype=int))
-    state = jnp.tensordot(a=unitary, b=state, axes=(op_dims, state_dims))
+    n_qubits = int(np.log2(operator.size))
+    operator = operator.reshape(tuple(2 for _ in np.arange(n_qubits)))
+    op_dims = tuple(np.arange(operator.ndim // 2, operator.ndim, dtype=int))
+    state = jnp.tensordot(a=operator, b=state, axes=(op_dims, state_dims))
     new_state_dims = tuple(i for i in range(len(state_dims)))
     return jnp.moveaxis(a=state, source=new_state_dims, destination=state_dims)
 
@@ -61,6 +61,8 @@ def apply_gate(
     Arguments:
         state: State to operate on.
         gate: Gate(s) to apply.
+        values: A dictionary with parameter values.
+        op_type: The type of operation to perform: Unitary, Dagger or Jacobian.
 
     Returns:
         State after applying 'gate'.
