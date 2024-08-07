3888c6eb303a3c85ca014b843fc85e0623e140ce
Roland Guichard <roland.guichard@pasqal.com>
2024-08-07 11:25:39 +0200

    Remove spurious import.

--- horqrux/__init__.py
+++ horqrux/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .api import expectation
 from .apply import apply_gate, apply_operator
-from .circuit import QuantumCircuit, expectation, sample
+from .circuit import QuantumCircuit, sample
 from .parametric import PHASE, RX, RY, RZ
 from .primitive import NOT, SWAP, H, I, S, T, X, Y, Z
 from .utils import (
