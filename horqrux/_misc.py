c96870a117a7f8ad82f3b4f8ab4a9e9657f17877
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-12-05 15:33:40 +0100

    lint

--- horqrux/_misc.py
+++ horqrux/_misc.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 import jax
 import jax.numpy as jnp
 from jax._src.typing import DType
+
 
 def default_complex_dtype() -> DType:
     if jax.config.jax_enable_x64:
