6f49beb7135ccf90b593c093abed3ce0fed5f3f4
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-12-06 08:50:00 +0100

    fix union

--- horqrux/utils.py
+++ horqrux/utils.py
@@ -18,7 +18,7 @@ State = ArrayLike
 QubitSupport = Tuple[Any, ...]
 ControlQubits = Tuple[Union[None, Tuple[int, ...]], ...]
 TargetQubits = Tuple[Tuple[int, ...], ...]
-ErrorProbabilities = Tuple[float, ...] | float
+ErrorProbabilities = Union[Tuple[float, ...], float]
 
 ATOL = 1e-014
 
