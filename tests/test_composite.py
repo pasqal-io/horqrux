c2ec1492f0e0e5cf2ab5c38a502a845323cb9006
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-02-14 17:45:56 +0100

    checking density mat input for sequence

--- tests/test_composite.py
+++ tests/test_composite.py
@@ -11,9 +11,9 @@ import random
 from horqrux.apply import apply_gate, apply_operator
 from horqrux.primitives.parametric import PHASE, RX, RY, RZ
 from horqrux.primitives.primitive import NOT, H, I, S, T, X, Y, Z
-from horqrux.composite import Scale, Add, Sequence
+from horqrux.composite import Scale, Add, Sequence, Observable
 from horqrux.circuit import QuantumCircuit
-from horqrux.utils import random_state
+from horqrux.utils import random_state, density_mat
 
 MAX_QUBITS = 7
 PARAMETRIC_GATES = (RX, RY, RZ, PHASE)
@@ -31,6 +31,27 @@ def test_scale(gate_fn: Callable) -> None:
     scale_state = scale_gate(orig_state)
     assert jnp.allclose(
         jnp.array(2.0) * state, scale_state
+    )
+
+@pytest.mark.parametrize("gate_fn", PRIMITIVE_GATES)
+def test_observable_gate(gate_fn: Callable) -> None:
+    target = np.random.randint(0, MAX_QUBITS)
+    gate = gate_fn(target)
+    obs_gate = Observable([gate])
+    orig_state = random_state(MAX_QUBITS)
+
+    state = apply_gate(orig_state, gate)
+    obs_state = obs_gate(orig_state)
+    assert jnp.allclose(
+        state, obs_state
+    )
+
+    # test density matrix
+    orig_state = density_mat(orig_state)
+    state = apply_gate(orig_state, gate)
+    obs_state = obs_gate(orig_state)
+    assert jnp.allclose(
+        state.array, obs_state.array
     )
 
 def test_sequence() -> None:
@@ -57,6 +78,14 @@ def test_sequence() -> None:
     qc_output = qc(orig_state, values)
     assert jnp.allclose(
         qc_output, sequence_output
+    )
+
+    # test density matrix
+    orig_state = density_mat(orig_state)
+    sequence_output = circuit(orig_state, values)
+    apply_output = apply_gate(orig_state, ops, values)
+    assert jnp.allclose(
+        sequence_output.array, apply_output.array
     )
 
 
