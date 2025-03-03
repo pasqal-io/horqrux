7dfe5038c2a8ba6f3b1e657d3f77ab2f9c021e29
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-03 17:42:14 +0100

    test shots with gpsr

--- tests/test_shots.py
+++ tests/test_shots.py
@@ -11,6 +11,7 @@ from horqrux.primitives.primitive import Z
 from horqrux.utils import density_mat
 
 N_QUBITS = 2
+GPSR_ATOL = 0.0001
 SHOTS_ATOL = 0.01
 N_SHOTS = 100_000
 
@@ -29,6 +30,14 @@ def test_shots() -> None:
     def exact_dm(x):
         values = {"theta": x}
         return expectation(density_mat(state), circuit, observables, values, diff_mode="ad")
+
+    def exact_gpsr(x):
+        values = {"theta": x}
+        return expectation(state, circuit, observables, values, diff_mode="gpsr")
+
+    def exact_gpsr_dm(x):
+        values = {"theta": x}
+        return expectation(density_mat(state), circuit, observables, values, diff_mode="gpsr")
 
     def shots(x):
         values = {"theta": x}
@@ -62,6 +71,13 @@ def test_shots() -> None:
     exp_exact_dm = exact_dm(x)
     assert jnp.allclose(exp_exact, exp_exact_dm)
 
+    exp_exact_gpsr = exact_gpsr(x)
+    exp_exact_gpsr_dm = exact_gpsr_dm(x)
+    assert jnp.allclose(exp_exact_gpsr, exp_exact_gpsr_dm)
+
+    assert jnp.allclose(exp_exact, exp_exact_gpsr, atol=GPSR_ATOL)
+    assert jnp.allclose(exp_exact_dm, exp_exact_gpsr_dm, atol=GPSR_ATOL)
+
     exp_shots = shots(x)
     exp_shots_dm = shots_dm(x)
 
@@ -69,9 +85,13 @@ def test_shots() -> None:
     assert jnp.allclose(exp_exact, exp_shots_dm, atol=SHOTS_ATOL)
 
     d_exact = jax.grad(lambda x: exact(x).sum())
+    d_gpsr = jax.grad(lambda x: exact_gpsr(x).sum())
     d_shots = jax.grad(lambda x: shots(x).sum())
 
     grad_backprop = d_exact(x)
+    grad_gpsr = d_gpsr(x)
+    assert jnp.isclose(grad_backprop, grad_gpsr, atol=GPSR_ATOL)
+
     grad_shots = d_shots(x)
 
     assert jnp.isclose(grad_backprop, grad_shots, atol=SHOTS_ATOL)
