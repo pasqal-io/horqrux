38552a123ac3fc1a938c783a627cc74da3ebbf51
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-03 16:34:18 +0100

    put gpsr separate

--- horqrux/differentiation/__init__.py
+++ horqrux/differentiation/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from .adjoint import adjoint_expectation
 from .automatic_diff import ad_expectation
-from .shots import finite_shots_fwd
+from .gpsr.shots import finite_shots_fwd
