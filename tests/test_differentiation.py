f924d6acb37617a5d9496f481cc11b50fa1a5217
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-05 11:43:22 +0100

    make parameterize test + add extra op in differentiation

--- tests/test_differentiation.py
+++ tests/test_differentiation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import jax
 import jax.numpy as jnp
+import pytest
 
 from horqrux import expectation, random_state, run
 from horqrux.circuit import QuantumCircuit
@@ -16,31 +17,39 @@ SHOTS_ATOL = 0.01
 N_SHOTS = 100_000
 
 
-def test_shots() -> None:
-    ops = [RX("theta", 0)]
+@pytest.mark.parametrize(
+    "same_name",
+    [
+        False,
+    ],
+)
+def test_shots(same_name: bool) -> None:
+    param_names = ["theta", "theta"] if same_name else ["theta", "theta2"]
+
+    ops = [RX(param_names[0], 0), RX(param_names[1], 1)]
     circuit = QuantumCircuit(2, ops)
     observables = [Observable([Z(0)]), Observable([Z(1)])]
     state = random_state(N_QUBITS)
-    x = jnp.pi * 0.5
+    x = jax.random.uniform(jax.random.key(0), (2))
 
     def exact(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(state, circuit, observables, values, diff_mode="ad")
 
     def exact_dm(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(density_mat(state), circuit, observables, values, diff_mode="ad")
 
     def exact_gpsr(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(state, circuit, observables, values, diff_mode="gpsr")
 
     def exact_gpsr_dm(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(density_mat(state), circuit, observables, values, diff_mode="gpsr")
 
     def shots(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(
             state,
             circuit,
@@ -52,7 +61,7 @@ def test_shots() -> None:
         )
 
     def shots_dm(x):
-        values = {"theta": x}
+        values = {param_names[0]: x[0], param_names[1]: x[1]}
         return expectation(
             density_mat(state),
             circuit,
@@ -63,8 +72,8 @@ def test_shots() -> None:
             n_shots=N_SHOTS,
         )
 
-    expected_dm = density_mat(run(circuit, state, {"theta": x}))
-    output_dm = run(circuit, density_mat(state), {"theta": x})
+    expected_dm = density_mat(run(circuit, state, {"theta": x[0], "theta2": x[1]}))
+    output_dm = run(circuit, density_mat(state), {"theta": x[0], "theta2": x[1]})
     assert jnp.allclose(expected_dm.array, output_dm.array)
 
     exp_exact = exact(x)
@@ -90,8 +99,8 @@ def test_shots() -> None:
 
     grad_backprop = d_exact(x)
     grad_gpsr = d_gpsr(x)
-    assert jnp.isclose(grad_backprop, grad_gpsr, atol=GPSR_ATOL)
+    assert jnp.allclose(grad_backprop, grad_gpsr, atol=GPSR_ATOL)
 
     grad_shots = d_shots(x)
 
-    assert jnp.isclose(grad_backprop, grad_shots, atol=SHOTS_ATOL)
+    assert jnp.allclose(grad_backprop, grad_shots, atol=SHOTS_ATOL)
