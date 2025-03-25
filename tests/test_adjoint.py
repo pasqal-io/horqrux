33640852cd94759ac1bc829bb9e8c8ab3a9333c9
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-25 10:03:16 +0100

    test adjoint with expectaion raised

--- tests/test_adjoint.py
+++ tests/test_adjoint.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import unittest
+
+import jax
 import numpy as np
-from jax import Array, grad
+from jax import Array, value_and_grad
 
 from horqrux import expectation, random_state
 from horqrux.circuit import QuantumCircuit
@@ -17,35 +20,41 @@ PARAMETRIC_GATES = (RX, RY, RZ, PHASE)
 PRIMITIVE_GATES = (NOT, H, X, Y, Z, I, S, T)
 
 
-def test_gradcheck() -> None:
-    ops = [RX("theta", 0), RY("epsilon", 0), RX("phi", 0), NOT(1, 0), RX("omega", 0, 1)]
-    circuit = QuantumCircuit(2, ops)
-    circuit_sparse = to_sparse(circuit)
-    observable = [Observable([Z(0)])]
-    observable_sparse = [to_sparse(observable[0])]
-    values = {
-        "theta": np.random.uniform(0, 1),
-        "epsilon": np.random.uniform(0, 1),
-        "phi": np.random.uniform(0, 1),
-        "omega": np.random.uniform(0, 1),
-    }
-    state = random_state(MAX_QUBITS)
-    state_sparse = random_state(MAX_QUBITS, sparse=True)
+class TestAdjoint(unittest.TestCase):
+    def test_gradcheck(self) -> None:
+        ops = [RX("theta", 0), RY("epsilon", 0), RX("phi", 0), NOT(1, 0), RX("omega", 0, 1)]
+        circuit = QuantumCircuit(2, ops)
+        circuit_sparse = to_sparse(circuit)
+        observable = [Observable([Z(0)])]
+        observable_sparse = [to_sparse(observable[0])]
+        values = {
+            "theta": np.random.uniform(0, 1),
+            "epsilon": np.random.uniform(0, 1),
+            "phi": np.random.uniform(0, 1),
+            "omega": np.random.uniform(0, 1),
+        }
+        state = random_state(MAX_QUBITS)
+        state_sparse = random_state(MAX_QUBITS, sparse=True)
 
-    def exp_fn(values: dict, diff_mode: DiffMode = "ad") -> Array:
-        return expectation(state, circuit, observable, values, diff_mode).item()
+        def exp_fn(values: dict, diff_mode: DiffMode = "ad") -> Array:
+            return expectation(state, circuit, observable, values, diff_mode).sum()
 
-    def exp_fn_sparse(values: dict, diff_mode: DiffMode = "ad") -> Array:
-        return (
-            expectation(state_sparse, circuit_sparse, observable_sparse, values, diff_mode)
-            .todense()
-            .item()
-        )
+        def exp_fn_sparse(values: dict, diff_mode: DiffMode = "ad") -> Array:
+            return expectation(
+                state_sparse, circuit_sparse, observable_sparse, values, diff_mode
+            ).sum()
 
-    grad_ad = grad(exp_fn)(values)
-    grads_adjoint = grad(exp_fn)(values, "adjoint")
-    grads_adjoint_sparse = grad(exp_fn_sparse)(values, "adjoint")
+        exp_ad, grad_ad = value_and_grad(exp_fn)(values)
+        exp_adjoint, grads_adjoint = value_and_grad(exp_fn)(values, "adjoint")
+        assert verify_arrays(exp_ad, exp_adjoint)
 
-    for param, ad_grad in grad_ad.items():
-        assert verify_arrays(grads_adjoint[param], ad_grad, atol=1.0e-3)
-        assert verify_arrays(grads_adjoint_sparse[param], grads_adjoint[param])
+        # note grad does not work for sparse operations
+        exp_adjoint_sparse = exp_fn_sparse(values, "adjoint")
+        with self.assertRaises(NotImplementedError):
+            jax.experimental.sparse.grad(exp_fn_sparse)(values, "adjoint")
+
+        assert verify_arrays(exp_adjoint, exp_adjoint_sparse)
+
+        for param, ad_grad in grad_ad.items():
+            assert verify_arrays(grads_adjoint[param], ad_grad, atol=1.0e-3)
+            # assert verify_arrays(grads_adjoint_sparse[param], grads_adjoint[param])
