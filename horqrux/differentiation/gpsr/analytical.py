ed095d394a27aba8984bd4e6ea76aca952ad0285
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-05-22 17:08:30 +0200

    no shots with scan for non repeated case

--- horqrux/differentiation/gpsr/analytical.py
+++ horqrux/differentiation/gpsr/analytical.py
@@ -11,7 +11,6 @@ from horqrux.apply import apply_gates
 from horqrux.composite import Observable
 from horqrux.differentiation.ad import _ad_expectation_single_observable
 from horqrux.differentiation.gpsr.gpsr_utils import (
-    alter_gate_sequence,
     extract_gate_names,
     initialize_gpsr_ingredients,
     prepare_param_gates_seq,
@@ -20,113 +19,6 @@ from horqrux.differentiation.gpsr.gpsr_utils import (
 from horqrux.primitives import Primitive
 from horqrux.utils.operator_utils import State
 from horqrux.utils.sparse_utils import stack_sp
-
-
-def no_shots(
-    state: State,
-    gates: Union[Primitive, Iterable[Primitive]],
-    observables: list[Observable],
-    values: dict[str, float],
-) -> Array:
-    """Run 'state' through a sequence of 'gates' given parameters 'values'
-    and compute the expectations analytically.
-
-    Args:
-        state (State): Input state or density matrix.
-        gates (Union[Primitive, Iterable[Primitive]]): Sequence of gates.
-        observables (list[Observable]): List of observables.
-        values (dict[str, float]): Parameter values.
-
-    Returns:
-        Array: Expectation values.
-    """
-    outputs = list(
-        map(
-            lambda observable: _ad_expectation_single_observable(
-                apply_gates(state, gates, values),
-                observable,
-                values,
-            ),
-            observables,
-        )
-    )
-    return stack_sp(outputs)
-
-
-jitted_no_shots = jax.jit(no_shots)
-
-
-def no_shots_bwd(
-    state: State,
-    gates: Union[Primitive, Iterable[Primitive]],
-    observables: list[Observable],
-    values: dict[str, float],
-) -> Array:
-    """Compute the gradient using the GPSR algorithm.
-    This function is efficient when used in a
-    training loop using the jitted version
-    of the `no_shots` function
-    (that is the `horqrux.differentiation.gpsr.jitted_no_shots`).
-
-    There is also a jvp function `no_shots_fwd`compatible with `jax.grad`
-    but compilation time are long when using `jax.jit`.
-
-    Args:
-        state (State): Input state or density matrix.
-        gates (Union[Primitive, Iterable[Primitive]]): Sequence of gates.
-        observables (list[Observable]): List of observables.
-        values (dict[str, float]): Parameter values.
-
-    Returns:
-        Array: Gradients via the GPSR algorithm.
-    """
-    val_keys, spectral_gap, shift = initialize_gpsr_ingredients(values)
-
-    def jvp_component(param_name: str) -> Array:
-        up_val = values.copy()
-        up_val[param_name] = up_val[param_name] + shift
-        f_up = jitted_no_shots(state, gates, observables, up_val)
-        down_val = values.copy()
-        down_val[param_name] = down_val[param_name] - shift
-        f_down = jitted_no_shots(state, gates, observables, down_val)
-        grad = (
-            spectral_gap[param_name]
-            * (f_up - f_down)
-            / (4.0 * jnp.sin(spectral_gap[param_name] * shift / 2.0))
-        )
-        return grad
-
-    jvp_caller = jvp_component
-    if not isinstance(gates, Primitive):
-        gate_names = extract_gate_names(gates)
-        if len(gate_names) > len(val_keys):
-            param_to_gates_indices = prepare_param_gates_seq(val_keys, gates)
-            # repeated case
-            if max(map(len, param_to_gates_indices.values())) > 1:  # type: ignore[arg-type]
-
-                def jvp_component_repeated_param(param_name: str) -> Array:
-                    shift_gates = param_to_gates_indices[param_name]
-
-                    def shift_jvp(ind: int) -> Array:
-                        spectral_gap = gates[ind].spectral_gap  # type: ignore[index]
-                        gates_up = alter_gate_sequence(gates, ind, shift)
-                        f_up = jitted_no_shots(state, gates_up, observables, values)
-                        gates_down = alter_gate_sequence(gates, ind, -shift)
-                        f_down = jitted_no_shots(state, gates_down, observables, values)
-                        return (
-                            spectral_gap
-                            * (f_up - f_down)
-                            / (4.0 * jnp.sin(spectral_gap * shift / 2.0))
-                        )
-
-                    return sum(shift_jvp(shift_ind) for shift_ind in shift_gates)
-
-                jvp_caller = jvp_component_repeated_param
-            else:
-                spectral_gap = spectral_gap_from_gates(param_to_gates_indices, val_keys)
-
-    jvp = jnp.stack([jvp_caller(param) for param in val_keys]).sum(axis=-1)
-    return jvp
 
 
 @partial(jax.custom_jvp, nondiff_argnums=(0, 1, 2))
@@ -154,7 +46,17 @@ def no_shots_fwd(
     Returns:
         Array: Expectation values.
     """
-    return no_shots(state, gates, observables, values)
+    outputs = list(
+        map(
+            lambda observable: _ad_expectation_single_observable(
+                apply_gates(state, gates, values),
+                observable,
+                values,
+            ),
+            observables,
+        )
+    )
+    return stack_sp(outputs)
 
 
 @no_shots_fwd.defjvp
@@ -181,52 +83,44 @@ def no_shots_fwd_jvp(
     """
     values = primals[0]
     tangent_dict = tangents[0]
+    fwd = no_shots_fwd(state, gates, observable, values)
 
     val_keys, spectral_gap, shift = initialize_gpsr_ingredients(values)
+    values_array = jnp.stack(list(values.values()))
 
-    def jvp_component(param_name: str) -> Array:
-        up_val = values.copy()
-        up_val[param_name] = up_val[param_name] + shift
+    def values_to_dict(x: Array) -> dict[str, Array]:
+        return dict(zip(val_keys, x))
+
+    def jvp_scan_norepeat(
+        carry_grad: Array, pytree_ind_param: dict[str, Array]
+    ) -> tuple[Array, Array]:
+        shift_vector = pytree_ind_param["shift"]
+        spectral_gap = pytree_ind_param["spectral_gap"]
+        up_val = values_to_dict(values_array + shift_vector)
         f_up = no_shots_fwd(state, gates, observable, up_val)
-        down_val = values.copy()
-        down_val[param_name] = down_val[param_name] - shift
+        down_val = values_to_dict(values_array - shift_vector)
         f_down = no_shots_fwd(state, gates, observable, down_val)
-        grad = (
-            spectral_gap[param_name]
-            * (f_up - f_down)
-            / (4.0 * jnp.sin(spectral_gap[param_name] * shift / 2.0))
-        )
-        return grad
+        grad = spectral_gap * (f_up - f_down) / (4.0 * jnp.sin(spectral_gap * shift / 2.0))
+        return carry_grad + grad, grad
 
-    fwd = no_shots_fwd(state, gates, observable, values)
-    jvp_caller = jvp_component
+    jvp_caller = jvp_scan_norepeat
     if not isinstance(gates, Primitive):
         gate_names = extract_gate_names(gates)
         if len(gate_names) > len(val_keys):
             param_to_gates_indices = prepare_param_gates_seq(val_keys, gates)
             # repeated case
             if max(map(len, param_to_gates_indices.values())) > 1:  # type: ignore[arg-type]
-
-                def jvp_component_repeated_param(param_name: str) -> Array:
-                    shift_gates = param_to_gates_indices[param_name]
-
-                    def shift_jvp(ind: int) -> Array:
-                        spectral_gap = gates[ind].spectral_gap  # type: ignore[index]
-                        gates_up = alter_gate_sequence(gates, ind, shift)
-                        f_up = no_shots_fwd(state, gates_up, observable, values)
-                        gates_down = alter_gate_sequence(gates, ind, -shift)
-                        f_down = no_shots_fwd(state, gates_down, observable, values)
-                        return (
-                            spectral_gap
-                            * (f_up - f_down)
-                            / (4.0 * jnp.sin(spectral_gap * shift / 2.0))
-                        )
-
-                    return sum(shift_jvp(shift_ind) for shift_ind in shift_gates)
-
-                jvp_caller = jvp_component_repeated_param
+                raise NotImplementedError("Repeated case not implemented yet")
             else:
                 spectral_gap = spectral_gap_from_gates(param_to_gates_indices, val_keys)
 
-    jvp = sum(jvp_caller(param) * tangent_dict[param] for param in val_keys)
+    spectral_gap_array = jnp.stack(list(spectral_gap.values()))
+    tangent_array = jnp.stack(list(tangent_dict.values()))
+    pytree_scan = {
+        "shift": shift * jnp.eye(len(val_keys), dtype=values_array.dtype),
+        "spectral_gap": spectral_gap_array,
+    }
+
+    _, grads = jax.lax.scan(jvp_caller, jnp.zeros(1), pytree_scan)
+    jvp = jnp.sum(jnp.stack(grads) * tangent_array.reshape(grads.shape))
     return fwd, jvp.reshape(fwd.shape)
